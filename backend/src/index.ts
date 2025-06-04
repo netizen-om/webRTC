b4ff6dbb45f07a6ebf51ab38a9b8a9a14c18c39f
netizen-om <borisagarom@gmail.com>
2025-06-04 22:41:29 +0530

    kinda completed backed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,37 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port : 3001 })
 
+let senderSocket: null | WebSocket = null;
+let reciverSocket: null | WebSocket = null;
+
 wss.on("connection", (ws) => {
+    console.log("Connected to WS server");
+    
     ws.on("error", (error) => console.log(error))
 
     ws.on("message", (data : any) => {
-        const msg = JSON.parse(data);
+        const messgae = JSON.parse(data);
+        
+        if(messgae.type === "identify-as-sender"){
+            senderSocket = ws;
+        } else if(messgae.type === "identify-as-reciver") {
+            reciverSocket = ws;
+        } else if(messgae.type === "create-offer") {
+            reciverSocket?.send(
+                JSON.stringify({
+                    type: "offer",
+                    offer : messgae.offer
+                })
+            )
+        } else if(messgae.type === "create-answer") {
+            senderSocket?.send(
+                JSON.stringify({
+                    type: "offer",
+                    offer : messgae.offer
+                })
+            )
+        }
+        
     })
 })
